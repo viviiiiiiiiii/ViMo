bd971679b18ef99421bbaccfbac44a563f343bb4
Mourad Majdouli <129498509+MouradMjd@users.noreply.github.com>
2026-04-18 19:25:22 +0200

    Version 1.5, ready for server

---
+++ run_index.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=vimo_indexing
+#SBATCH --partition=all_gpu       #CAMBIATO: Chiediamo un nodo con GPU
+#SBATCH --gres=gpu:1             #AGGIUNTO: Vogliamo 1 scheda video
+#SBATCH --cpus-per-task=4        #AUMENTATO: Più core per caricare i dati
+#SBATCH --mem=32G                #AUMENTATO: Almeno 32GB per EVA-CLIP-8B
+#SBATCH --time=01:00:00          # 1 ora basta e avanza per 3 documenti
+#SBATCH --account=cvcs2026
+#SBATCH --output=/homes/%u/cvcs2026/index_%j.out
+
+# Carica l'ambiente corretto (assicurati che il percorso sia giusto!)
+source /work/cvcs2026/ViMo/.venvMo/bin/activate
+
+# Entra nella cartella corretta
+cd /work/cvcs2026/ViMo/
+
+# Lancia lo script di indicizzazione
+export TRANSFORMERS_IGNORE_LOAD_VULNERABILITY=1
+python dati/build_knn_index_real.py
