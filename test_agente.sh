f3bbaf5ffb46b7d6baddd47c80dbf5bf7bf79b31
Mourad Majdouli <129498509+MouradMjd@users.noreply.github.com>
2026-05-13 21:14:26 +0200

    .sh agente con 64gb ram

--- test_agente.sh
+++ test_agente.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=all_usr_prod  # La partizione GPU che abbiamo trovato
 #SBATCH --gres=gpu:2             #Chiediamo 1 GPU
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=40G                  # Qwen2.5-VL ha bisogno di molta RAM
+#SBATCH --mem=64G                # Qwen2.5-VL ha bisogno di molta RAM
 #SBATCH --time=00:20:00
 #SBATCH --account=cvcs2026
 #SBATCH --output=/homes/%u/cvcs2026/agent_output_%j.out
