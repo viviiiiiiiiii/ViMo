dea7b0bd0b8aa3bf249f4bbcccbf7d25d94ec765
viviiiiiiiiii <maestri.virginia10@gmail.com>
2026-05-14 11:36:08 +0200

    try con worktree 1

--- create_evqa.sh
+++ create_evqa.sh
@@ -12,7 +12,7 @@
 source /work/cvcs2026/ViMo/.venvMo/bin/activate
 
 # Entra nella cartella corretta
-cd "$SLURM_SUBMIT_DIR"
+cd "$SLURM_SUBMIT_DIR"/dati
 
 # Lancia lo script di indicizzazione
 export CUDA_VISIBLE_DEVICES=""
