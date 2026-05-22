931831ac06db337125e7fbcd75976307a3d55209
viviiiiiiiiii <maestri.virginia10@gmail.com>
2026-05-22 12:59:56 +0200

    Try 1

--- run_predictions.sh
+++ run_predictions.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=all_usr_prod      #CAMBIATO: Chiediamo un nodo con GPU
 #SBATCH --gres=gpu:1             #AGGIUNTO: Vogliamo 1 scheda video
 #SBATCH --cpus-per-task=4        #AUMENTATO: Più core per caricare i dati
-#SBATCH --mem=32G                #AUMENTATO: Almeno 32GB per EVA-CLIP-8B
+#SBATCH --mem=64G                #AUMENTATO: Almeno 32GB per EVA-CLIP-8B
 #SBATCH --time=01:00:00          # 1 ora basta e avanza per 3 documenti
 #SBATCH --account=cvcs2026
 #SBATCH --output=/homes/%u/cvcs2026/index_%j.out
@@ -22,24 +22,24 @@ python run_prediction_vlm.py \
   --image-root /work/cvcs2026/encyclopedic \
   --pred-out predictions_vlm_test.jsonl \
   --records-out records_vlm_test.jsonl \
-  --limit 3
+  --limit 1
 
-  # Check: head predictions_vlm_test.jsonl  head records_vlm_test.jsonl
-
-  python run_prediction_baseline_rag.py \
-  --input /work/cvcs2026/encyclopedic/single_hop.json \
-  --image-root /work/cvcs2026/encyclopedic \
-  --pred-out predictions_rag_test.jsonl \
-  --records-out records_rag_test.jsonl \
-  --limit 3
-
-  # Check: head predictions_rag_test.jsonl  head records_rag_test.jsonl
-
-  python run_prediction_agentic.py \
-  --input /work/cvcs2026/encyclopedic/single_hop.json \
-  --image-root /work/cvcs2026/encyclopedic \
-  --pred-out predictions_agentic_test.jsonl \
-  --records-out records_agentic_test.jsonl \
-  --limit 3
-
-  # Check: head predictions_agentic_test.jsonl  head records_agentic_test.jsonl
+  ## Check: head predictions_vlm_test.jsonl  head records_vlm_test.jsonl
+#
+  #python run_prediction_baseline_rag.py \
+  #--input /work/cvcs2026/encyclopedic/single_hop.json \
+  #--image-root /work/cvcs2026/encyclopedic \
+  #--pred-out predictions_rag_test.jsonl \
+  #--records-out records_rag_test.jsonl \
+  #--limit 1
+#
+  ## Check: head predictions_rag_test.jsonl  head records_rag_test.jsonl
+#
+  #python run_prediction_agentic.py \
+  #--input /work/cvcs2026/encyclopedic/single_hop.json \
+  #--image-root /work/cvcs2026/encyclopedic \
+  #--pred-out predictions_agentic_test.jsonl \
+  #--records-out records_agentic_test.jsonl \
+  #--limit 3
+#
+  ## Check: head predictions_agentic_test.jsonl  head records_agentic_test.jsonl
