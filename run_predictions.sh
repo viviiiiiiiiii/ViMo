b8f0fde9bc2d30ccc390c842e0d80d5163697b93
viviiiiiiiiii <maestri.virginia10@gmail.com>
2026-05-22 17:54:08 +0200

    Try

--- run_predictions.sh
+++ run_predictions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=vimo_agent_test
+#SBATCH --job-name=vimo_predictions_test
 #SBATCH --partition=all_usr_prod  # La partizione GPU che abbiamo trovato
 #SBATCH --gres=gpu:2             #Chiediamo 1 GPU
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G                # Qwen2.5-VL ha bisogno di molta RAM
 #SBATCH --time=00:20:00
 #SBATCH --account=cvcs2026
-#SBATCH --output=/homes/%u/cvcs2026/agent_output_%j.out
+#SBATCH --output=/homes/%u/cvcs2026/predictions_%j.out
 
 
 # Carica l'ambiente
